ceb744d30f1780822c2cd0bfdae7819cbef5d5ed
Jingtao Xu <jingtaozf@gmail.com>
2021-01-01 19:13:14 +0800

    update emacs config

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,5 +1,6 @@
 ;;; -*- encoding:utf-8 -*-  ---
 ((nil
   (toc-org-max-depth . 3)
+  (tab-width . 2)
   (org-link-file-path-type . relative)
   (eval add-hook 'before-save-hook #'delete-trailing-whitespace nil t)))
